1f4c153123b33dd9f1c6dd1b41b072f6f68f5c86
Daekyoung Lim <daekyoung@hotmail.com>
2022-07-02 16:26:08 +0900

    addition of external program excution example

---
+++ SP_basic/ExternProgram/ExcuteExternProgram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace ExcuteExternProgram
+{
+	class ExtProc
+	{
+		public void workerExternProc()
+		{
+			ProcessStartInfo psi = new ProcessStartInfo
+			{
+				FileName = @"./../../../ExternProgram.exe",
+				Arguments = "args",
+				RedirectStandardOutput = true,
+				RedirectStandardInput = true,
+				UseShellExecute = false
+			};
+
+			Process proc = Process.Start(psi);
+			string strRet = proc.StandardOutput.ReadLine();
+			Console.WriteLine(strRet);
+
+			while (true)
+			{
+				String line = Console.ReadLine();
+				proc.StandardInput.WriteLine(line);
+
+				string strLine = proc.StandardOutput.ReadLine();
+				Console.WriteLine(strLine);
+			}
+		}
+	}
+
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			ExtProc extProc = new ExtProc();
+			
+			Thread th = new Thread(extProc.workerExternProc);
+
+			th.Start();
+			th.Join();
+		}
+    }
+}
---
+++ SP_basic/ExternProgram/ExternProgram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace ExternProgram
+{
+    class Echo
+    {
+        public void worker()
+        {
+            while (true)
+            {
+                string line = Console.ReadLine();
+                Console.WriteLine("[Echo]" + line);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] != null)
+            {
+                Console.WriteLine("[Echo]{0}", args[0]);
+            }
+
+            Echo echo = new Echo();
+
+            Thread th = new Thread(echo.worker);
+
+            th.Start();
+            th.Join();
+        }
+    }
+}
